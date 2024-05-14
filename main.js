79065b1c115301dfc1fb577db70ec1bbab7d6223
Dosonov Aibol <you@aiboldosonob@gmail.com>
2024-05-14 22:58:42 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const gifSection = document.querySelector('.gif-section')
+const searchBtn = document.querySelector('#search-btn')
+let apiKey = 'FF5JPMvjibp8AcaszNexYGxzFPnjRJ4n'
+
+
+const img = document.querySelector('.test-img')
+
+async function getGif()
+{
+    const deleteGif = document.querySelectorAll('#gif')
+    deleteGif.forEach((delGif)=>{
+        delGif.remove()
+    })
+    console.log(deleteGif)
+
+    const inputValue = document.querySelector('#search-box').value
+    const gifData = {
+        limit: 9,
+        offset: 0,
+        rating: 'g',
+    }
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${inputValue}&limit=6`
+    
+    const responce = await fetch(url)
+
+    const product = await responce.json()
+    const gifArray = product.data
+    gifArray.forEach((info)=>{
+        const gifContainer = `
+            <div id="gif" class="gif-container">
+                <img class="test-img" src="${info.images.downsized_medium.url}" alt="gif">
+            </div>
+        `
+        gifSection.insertAdjacentHTML('afterbegin', gifContainer)
+    })
+
+
+
+    // img.setAttribute('src', product.data[0].images.downsized_medium.url)
+}
+
+searchBtn.addEventListener('click', (event)=>{
+    event.preventDefault()
+    getGif()
+})
+window.addEventListener('load', getGif)
