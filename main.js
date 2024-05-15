8151c88a79f62ff73a0ca9ce7b1b3abb8694204a
Dosonov Aibol <you@aiboldosonob@gmail.com>
2024-05-15 14:49:31 +0600

    add adaptivity

--- main.js
+++ main.js
@@ -7,31 +7,33 @@ const img = document.querySelector('.test-img')
 async function getGif()
 {
     const deleteGif = document.querySelectorAll('#gif')
-    deleteGif.forEach((delGif)=>{
-        delGif.remove()
+    deleteGif.forEach((element)=>{
+        element.remove()
     })
-    console.log(deleteGif)
 
     const inputValue = document.querySelector('#search-box').value
     const gifData = {
-        limit: 9,
+        limit: 6,
         offset: 0,
         rating: 'g',
     }
-    let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${inputValue}&limit=6`
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${inputValue}&limit=${gifData.limit}&offset=${gifData.offset}&rating=${gifData.rating}`
+    try{
+        const responce = await fetch(url)
+        const product = await responce.json()
     
-    const responce = await fetch(url)
-
-    const product = await responce.json()
-    const gifArray = product.data
-    gifArray.forEach((info)=>{
-        const gifContainer = `
-            <div id="gif" class="gif-container">
-                <img class="test-img" src="${info.images.downsized_medium.url}" alt="gif">
-            </div>
-        `
-        gifSection.insertAdjacentHTML('afterbegin', gifContainer)
-    })
+        const gifArray = product.data
+        gifArray.forEach((info)=>{
+            const gifContainer = `
+                <div id="gif" class="gif-container">
+                    <img class="test-img" src="${info.images.downsized_medium.url}" alt="gif">
+                </div>
+            `
+            gifSection.insertAdjacentHTML('afterbegin', gifContainer)
+        })
+    }catch (err){
+        console.log(err.message)
+    }
 }
 
 searchBtn.addEventListener('click', (event)=>{
